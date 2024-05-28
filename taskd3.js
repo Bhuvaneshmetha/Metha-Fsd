e4b98c4389e092480e0a4705c9af9d02a7720eee
unknown <bhuvanes142004@gmail.com>
2024-05-28 09:29:10 +0530

    commit 2 file

--- taskd3.js
+++ taskd3.js
@@ -4,11 +4,5 @@ const obj = {
 }
 let keys = Object.keys(obj);
 console.log(keys);
-for (let key of keys){
-    console.log(key,obj[key])
-}
-for (let key in obj){
-    console.log(key,obj[key])
-}  
 console.log(obj.name,obj.age)
 console.log(obj.age,obj.name)
